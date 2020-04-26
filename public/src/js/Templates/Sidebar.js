21ad2317878b748a6072d222178de32f0146f396
liam <dedypips@gmail.com>
2020-04-26 12:29:22 -0600

    Made the sidebar a bit more mobile friendly.

--- public/src/js/Templates/Sidebar.js
+++ public/src/js/Templates/Sidebar.js
@@ -3,9 +3,18 @@ import './sidebar.css';
 
 const template = `
 <div class="sidebar">
-    <div class="sidebar-item" v-on:click="navigate('/')">home</div>
-    <div class="sidebar-item" v-on:click="navigate('/dice_roller')" to="/dice_roller">Dice Roller</div>
-    <div class="sidebar-item" v-on:click="navigate('/weapon_generator')">Weapon Generator</div>
+    <div class="sidebar-item" v-on:click="navigate('/')">
+        <span class="sidebar-icon" ><i class="fas fa-home"></i></span>
+        <span class="sidebar-text" >home</span>
+    </div>
+    <div class="sidebar-item" v-on:click="navigate('/dice_roller')" to="/dice_roller">
+        <span class="sidebar-icon" ><i class="fas fa-dice-six"></i></span>
+        <span class="sidebar-text" >Dice Roller</span>
+    </div>
+    <div class="sidebar-item" v-on:click="navigate('/weapon_generator')">
+        <span class="sidebar-icon" ><i class="fas fa-cog" ></i></span>
+        <span class="sidebar-text" >Weapon Generator</span>
+    </div>
 </div>
 `;
 
