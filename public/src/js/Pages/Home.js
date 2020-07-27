cd6e0b86d1a3070a98aff8a10c8bd9ae6a84e4f0
liam <dedypips@gmail.com>
2020-07-27 14:50:13 -0600

    Added some styles, fixed a few misc things up.

--- public/src/js/Pages/Home.js
+++ public/src/js/Pages/Home.js
@@ -8,7 +8,7 @@ const template = `
         <sidebar></sidebar>
         <div class="homePage">
             <div class="mainContent" >
-                <h1>Homepage</h1>
+                <h1>Liam Johnson</h1>
                 <p>Hello! I'm Liam - this is my website - please do have a look around while you're here. I'm a full-stack developer with experience in a fair amount of web development languages and frameworks. I know my way around React JS the best, but am practiced in vanilla JavaScript, CSS, and Vue JS as well. For back-end languages I mainly work with vanilla PHP to get the job done. This particular website's UI was built using Vue JS and Vue Router - along with vanilla css for styling. Server routing is being taken care of with the PHP's Slim Framework and a tiny templating library. If you're interesting in perusing the repository you can find it on <a href='https://github.com/00-status/liam-j' target='_blank' rel='noopener noreferrer'>GitHub</a>. </p>
                 <p>I can be a pretty nerdy. Video games, Star Wars, superhero movies – if it's sci-fi or fantasy I'm probably into it. I'm particularly fond of Dungeons and Dragons and other tabletop games. I think they can be an excellent way to connect with others, and they've also been a great source of creative inspiration in my personal projects as well – as you may guess from some of the other pages on this site.</p>
                 <p>If you'd like to get in contact with me, <a href='mailto:liam.jj@hotmail.com' target='_blank' rel='noopener noreferrer' >liam.jj@hotmail.com</a> is the best place to do it, but you're welcome to check out any of my social media platforms as well. </p>
--- public/src/js/WeaponGenerator/WeaponGeneratorPage.js
+++ public/src/js/WeaponGenerator/WeaponGeneratorPage.js
@@ -4,9 +4,9 @@ import WeaponGenerator from "./WeaponGenerator";
 const template = `
 <page>
     <template v-slot:information >
-        <h1>Weapon Generator</h1>
+        <h1>Weapon Generator!</h1>
         <p>This guy is a weapon generator for Dungeons and Dragons 5th Edition. It works by randomly generating a weapon name from a list of words, and then deriving points from that name. These points are then used to make up the various special effects attached to that weapon. Originally I had planned to include a custom backstory with each item, but I quickly found doing so would inflate the scope of the project just a little too much. Regardless, I feel like it can still be a good source of inspiration for anybody who may be running a D&D game of their own.</p>
-        <p>The UI was built using vanilla css along with Vue JS, and the api used to generate a weapon was done using Slim PHP. If you're curious as to how it was made, feel free to take a look at the <a href='https://github.com/00-status/weapon-generator' target='_blank' rel='noopener noreferrer'>GitHub repository</a>.</p>
+        <p>The UI was built using vanilla CSS along with Vue JS, and the api used to generate a weapon was done using Slim PHP. If you're curious as to how it was made, feel free to take a look at the <a href='https://github.com/00-status/weapon-generator' target='_blank' rel='noopener noreferrer'>GitHub repository</a>.</p>
     </template>
     <template v-slot:widget >
         <weapon-generator></weapon-generator>
